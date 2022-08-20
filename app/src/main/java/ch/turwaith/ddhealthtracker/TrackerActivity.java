a6e728cea0d9f50fd3049bb47f013c0667f9a8d0
Turwaith <nicolasbaur@turwaith.dev>
2022-08-20 15:56:29 +0200

    Adds project files Adds two new activities

---
+++ app/src/main/java/ch/turwaith/ddhealthtracker/TrackerActivity.java
@@ -0,0 +1,14 @@
+package ch.turwaith.ddhealthtracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TrackerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tracker);
+    }
+}
